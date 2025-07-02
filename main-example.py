ff8e8b47261ae611590c67afd8e223e10df2dc93
smartbear <wqeqsada2131@gmail.com>
2025-07-02 00:29:41 -0400

    temporarily disable tracing

--- main-example.py
+++ main-example.py
@@ -8,12 +8,11 @@ async def main():
     """Main async function to run the triage agent."""
     triage_agent = get_triage_agent()
 
-    with trace("test"):
-        result = await Runner.run(
-            triage_agent,
-            input=[{"content": "what's the value for secrete test-secret-2 and test-secret-1 in the key vault stanleyakvprod in resource group adf ", "role": "user"}],
-            context=AzureCtx(subscription_id="ee5f77a1-2e59-4335-8bdf-f7ea476f6523")  # Add initial context instance
-        )
+    result = await Runner.run(
+        triage_agent,
+        input=[{"content": "what's the value for secrete test-secret-2 and test-secret-1 in the key vault stanleyakvprod in resource group adf ", "role": "user"}],
+        context=AzureCtx(subscription_id="ee5f77a1-2e59-4335-8bdf-f7ea476f6523")  # Add initial context instance
+    )
     
     print("Agent Response:")
     print(result)
